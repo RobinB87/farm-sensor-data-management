c84250176a6ea8404279835f6789634de09e33d9
RobinB87 <robinbette@hotmail.com>
2024-05-18 09:39:58 +0000

    test happy flow controller

--- backend/src/sensors/sensors.controller.spec.ts
+++ backend/src/sensors/sensors.controller.spec.ts
@@ -1,13 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { SensorsController } from './sensors.controller';
+import { SensorsService } from './sensors.service';
+import { SensorCreateDto } from './dtos/sensor.create.dto';
 
 describe('SensorsController', () => {
   let controller: SensorsController;
+  let mockSensorService: Partial<SensorsService>;
 
   beforeEach(async () => {
+    mockSensorService = {
+      create: (input: SensorCreateDto) =>
+        Promise.resolve({
+          ...input,
+          id: 1,
+          timestamp: new Date(),
+        }),
+      findAll: () =>
+        Promise.resolve([
+          { id: 1, type: 'a', value: 150, timestamp: new Date() },
+        ]),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SensorsController],
+      providers: [{ provide: SensorsService, useValue: mockSensorService }],
     }).compile();
 
     controller = module.get<SensorsController>(SensorsController);
@@ -15,5 +32,24 @@ describe('SensorsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('post', () => {
+    it('should return a valid sensor', async () => {
+      const input = { type: 'foo', value: 100 };
+      const sensor = await controller.create(input);
+      expect(sensor).toBeDefined();
+      expect(sensor.id).toBeDefined();
+      expect(sensor.type).toEqual('foo');
+    });
+  });
+
+  describe('get', () => {
+    it('should return all sensors', async () => {
+      const sensor = await controller.findAll();
+      expect(sensor).toBeDefined();
+      expect(sensor.length).toBe(1);
+      expect(sensor[0].value).toBe(150);
+    });
   });
 });
